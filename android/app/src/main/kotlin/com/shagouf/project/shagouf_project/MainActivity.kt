efd30a5ad2c622c0773e1c673c2bf144e284b1a3
Almuhannad Ghaleb <henno.noman@gmail.com>
2023-08-03 21:27:06 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/shagouf/project/shagouf_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shagouf.project.shagouf_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,201 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Row and Column With Scroll'),
+        ),
+        body: Row(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                    MaterialButton(
+                      color: Colors.red,
+                      child: const Text('First Button'),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
